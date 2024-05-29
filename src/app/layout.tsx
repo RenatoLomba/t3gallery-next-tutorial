326fd852938ee9327df7cc0e2b81f5fa781137c0
RenatoLomba <rntlomba@gmail.com>
2024-05-29 20:55:42 -0300

    signed in only content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" className={`${GeistSans.variable}`}>
-        <body className="flex flex-col gap-4">
+        <body className="flex h-screen flex-col gap-4">
           <Header />
 
-          <main>{children}</main>
+          <div className="h-full">{children}</div>
         </body>
       </html>
     </ClerkProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,38 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "../server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
-    <main>
-      <div className="flex flex-wrap gap-4">
-        {images.map((image) => (
-          <div key={"image_" + image.id} className="flex w-48 flex-col">
-            <img src={image.url} alt="" className="w-full" />
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={"image_" + image.id} className="flex w-48 flex-col">
+          <img src={image.url} alt="" className="w-full" />
 
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
+    <main className="h-full">
+      <SignedOut>
+        <div className="flex h-full w-full items-center justify-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
